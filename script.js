2fde1204c4789c478aba65cdb26ac062ee15b240
Dmitrii Voroshilov <Smarag87@gmail.com>
2021-11-25 08:22:43 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var range = document.getElementById("range").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * range /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    //Reference to the element "result". Tag <mark> was added to highlight the result numbers:
+    document.getElementById("result").innerHTML= "\<br\>\<br\>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+range+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
     
-}
         
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("range").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+// To chech numbers new function was added.
+function checkdata(){
+           var principal = document.getElementById("principal");
+                   
+            if(principal.value <= 0){
+                alert("Enter a positive number");
+                principal.focus();
+                return false;
+            }
+ //focus() element was created take the user back to the "Principal" input box.   
+            principal.focus();
+	    return true;
+	    
+        }
